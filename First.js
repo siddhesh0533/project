b4aa486af230836b666cc914ddeef54ac22bfd68
Sahilyadav18052003 <162944967+Sahilyadav18052003@users.noreply.github.com>
2024-10-03 12:56:12 +0530

    Add files via upload

---
+++ First.js
@@ -0,0 +1,15 @@
+Name = "SAHIL YADAV";
+age = 21;
+price = 99.99;
+a = null;
+let y;
+isfollow = true;
+let x = BigInt("123");
+let t = Symbol("Hello!");
+console.log(Name);
+console.log(price);
+console.log(isfollow);
+
+console.log("Hello JavaScript");
+console.log("Welcome to JavaScript");
+console.log("Sahil Yadav");
